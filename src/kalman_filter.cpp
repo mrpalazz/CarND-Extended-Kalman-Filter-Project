2928b4ab479568842da0e629bcf6e9b91b40ed20
mrpalazz <mrpal@LAPTOP-TLNM3T0P.localdomain>
2018-02-17 11:11:52 -0500

    updates

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,10 +1,11 @@
+#include <math.h>      
 #include "kalman_filter.h"
+# include <iostream>
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-// Please note that the Eigen library does not initialize 
-// VectorXd or MatrixXd objects with zeros upon creation.
+float pi = 3.1415;
 
 KalmanFilter::KalmanFilter() {}
 
@@ -25,18 +26,71 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
+//Laser Update
 void KalmanFilter::Update(const VectorXd &z) {
   /**
   TODO:
     * update the state by using Kalman Filter equations
   */
+  VectorXd y = z - H_ * x_;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //update the estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
+//Radar update
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+  float rho = sqrt(x_(0)*x_(0) + x_(1)*x_(1));
+  float phi = atan2(x_(1), x_(0));
+  std::cout << "\n\n"  <<  " The value of phi is equal to: " << phi << "\n\n" << std::endl;
+
+  float rho_dot;
+  if (fabs(rho) < 0.0001) {
+    rho_dot = 0;
+  } else {
+    rho_dot = (x_(0)*x_(2) + x_(1)*x_(3))/rho;
+  }
+  VectorXd z_pred(3);
+  z_pred << rho, phi, rho_dot;
+  VectorXd y = z - z_pred;
+
+ //Logic to adjust the phi value if it is greater than pi at the time of calculation
+  while (y(1) > pi) {
+	  std::cout << "/n/n" << "Phi is out of range and needs to be adjusted to between -pi and pi."<< "/n/n" << std::endl;
+	  y(1) = y(1) - pi;
+	  }
+	  
+  while (y(1) <  -pi) {
+	  std::cout << "\n\n"  <<  " jazz: " << phi << "\n\n" << std::endl;
+	  y(1) = y(1) + pi;
+	  }
+
+  MatrixXd Ht = H_.transpose(); 
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //update the estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
--- src/tools.cpp
+++ src/tools.cpp
@@ -15,6 +15,37 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+  VectorXd rmse(4);
+  
+  rmse << 0, 0, 0, 0;
+
+  // check following inputs are valid:
+  //  * the estimation vector should not be of length zero
+  //  * the estimation vector length must match the ground truth length
+  if (estimations.size() != ground_truth.size()
+      || estimations.size() == 0){
+      std::cout << "cannot proceed, the ground truth and estimation vector don't match" << std::endl;
+      return rmse;
+  }
+
+  //accumulate the squared residuals over the time steps
+  for (unsigned int i = 0; i < estimations.size(); ++i){
+
+      VectorXd residual = estimations[i] - ground_truth[i];
+
+      //coefficient-wise multiplication
+      residual = residual.array()*residual.array();
+      rmse += residual;
+  }
+
+  //calculate the mean
+  rmse = rmse / estimations.size();
+
+  //calculate the squared root
+  rmse = rmse.array().sqrt();
+
+  //return the result
+  return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
@@ -22,4 +53,32 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   TODO:
     * Calculate a Jacobian here.
   */
+  MatrixXd Hj(3, 4);
+  
+  //pull in the state parameters
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
+
+  //compute and store critical terms to avoid unneccesary calcualtions
+  float c1 = px*px + py*py;
+  float c2 = sqrt(c1);
+  float c3 = (c1*c2);
+
+  //check division by zero
+  if (fabs(c1) < 0.0001){
+      std::cout << " * Error * Division by Zero in Jacobian matrix" << std::endl;
+      return Hj;
+  }
+
+  //compute the Jacobian matrix
+  
+  Hj << (px / c2), (py / c2), 0, 0,
+  
+      -(py / c1), (px / c1), 0, 0,
+	  
+      py*(vx*py - vy*px) / c3, px*(px*vy - py*vx) / c3, px / c2, py / c2;
+
+  return Hj;
 }
